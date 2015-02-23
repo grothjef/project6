d568d0e91cb41a2ff0b2d7270a8942c873632ecf
Jeff Groth <jeffgroth@Jeffs-MacBook-Pro.local>
2015-02-23 12:47:22 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'holidapi'
+
+class MyWebApp < Sinatra::Base
+	get '/' do
+		@birthdays = HolidApi.get(country: 'US', year: 1992, month: 5)
+		@current = HolidApi.get(country: 'US', year: Time.now.year, month: Time.now.month)
+		erb :cats
+	end
+end
